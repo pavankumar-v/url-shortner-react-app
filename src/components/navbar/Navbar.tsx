9a12ad17ad242a2f89bcea80b0feb0187273ec22
Pavan Kumar V <pavankumarv124@gmail.com>
2025-05-11 13:41:13 +0530

    chore(navbar): integrate react-router-dom with redirect for nav links

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,20 @@
 import { Button } from '@/components/ui/button';
 import Text from '@/components/ui/text';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className='bg-primary border-b h-20'>
             <div className='mx-6 h-full flex items-center justify-between'>
-                <Text type='h2' className='text-white leading-none p-0 tracking-normal pb-0'>
-                    ⚡️ ShortUrl
-                </Text>
+                <Link to="/">
+                    <Text type='h2' className='text-white leading-none p-0 tracking-normal pb-0 cursor-pointer'>
+                        ⚡️ ShortUrl
+                    </Text>
+                </Link>
                 <div className='flex items-center gap-4'>
-                    <Button variant="default" className='font-medium'>Top 100</Button>
+                    <Link to="/top100">
+                        <Button variant="default" className='font-medium'>Top 100</Button>
+                    </Link>
                 </div>
             </div>
         </nav>
