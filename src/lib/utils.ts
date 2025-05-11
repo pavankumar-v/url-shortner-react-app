1562eff700305141a3fac3566a636ee8aeae3640
Pavan Kumar V <pavankumarv124@gmail.com>
2025-05-11 13:38:18 +0530

    chore(util): add helper to return short url from code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
+import { env } from "@/config/env"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getShortUrl(shortCode: string): string {
+  const shortUrlBase = env('VITE_SHORT_URL_BASE')
+  return `${shortUrlBase}/${shortCode}`
 }
