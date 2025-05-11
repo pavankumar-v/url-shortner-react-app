8902bfb6c8f45df295336cdd45bda20a0cd2b084
Pavan Kumar V <pavankumarv124@gmail.com>
2025-05-11 14:58:10 +0530

    feat(service): add service function to get top 100 urls

--- src/services/short-url.ts
+++ src/services/short-url.ts
@@ -1,10 +1,32 @@
 import { api } from "@/lib/api"
 
-type Response = {
+type ShortenResponse = {
   short_code: string;
 }
 
-export const shortenUrl = async (body: { full_url: string }): Promise<Response> => {
-  const response = await api.post<Response>('/short_urls', body)
+type UrlData = {
+  short_code: string;
+  full_url: string;
+  visit_count?: number;
+  created_at?: string;
+}
+
+type PaginatedUrlsResponse = {
+  urls: UrlData[];
+  metadata: {
+    total_records: number,
+    total_pages: number,
+    current_page: number,
+    per_page: number
+  }
+}
+
+export const shortenUrl = async (body: { full_url: string }): Promise<ShortenResponse> => {
+  const response = await api.post<ShortenResponse>('/short_urls', body)
   return response.data
+}
+
+export const getTop100Urls = async (page: number = 1): Promise<PaginatedUrlsResponse> => {
+  const response = await api.get<PaginatedUrlsResponse>(`/short_urls?page=${page}`);
+  return response.data;
 }
